58d66d362ce1944a303587423af56fd3efca938a
Ray Ng <ray.ng@gradeous.com.au>
2020-04-06 18:01:33 +1000

    fremove approle

--- ns_hashicorp/terraform.tf
+++ ns_hashicorp/terraform.tf
@@ -2,13 +2,4 @@ terraform {
   required_version = ">= 0.12.23"
 }
 
-provider "vault" {
-  auth_login {
-    path = "auth/approle/login"
-
-    parameters = {
-      role_id   = var.role_id
-      secret_id = var.secret_id
-    }
-  }
-}
+provider "vault" {}
